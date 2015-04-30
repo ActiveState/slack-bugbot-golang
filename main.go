230cc15891c120fb9b2934bc6b0c523e9a31c8ae
William Gautier <williamg@activestate.com>
2015-04-30 12:28:37 -0700

    Ignore own messages better For some reason bot messages aren't of "bot_message" subtype, only integration messages...

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
     for {
         event := <-chReceiver
         message, ok := event.Data.(*slack.MessageEvent)
-        if ok && message.SubType != "bot_message" { // If this is a MessageEvent
+        if ok && message.SubType != "bot_message" && message.UserId != botSlackId { // If this is a MessageEvent
             // That event doesn't contain the Username, so we can't use message.Username
             log.Printf("Message from %s in channel %s: %s\n", message.UserId, message.ChannelId, message.Text)
 
